69d935fffbc7f61cb337324dd4516a10233f7556
Gaspar <gasparcastello@gmail.com>
2022-04-28 11:58:50 -0300

    without login

--- src/App.js
+++ src/App.js
@@ -3,29 +3,16 @@ import { Route, Navigate, Routes } from "react-router-dom";
 
 import AuthContext from "./store/auth-context";
 import Layout from "./components/Layout/Layout";
-import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/SearchPage";
 
 function App() {
-  const authCtx = useContext(AuthContext);
-
   return (
     <Layout>
       <Routes>
-        {authCtx.isLoggedIn && (
-          <>
-            <Route path="*" element={<Navigate replace to="/home" />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/add-recipe" element={<SearchPage />} />
-          </>
-        )}
-        {!authCtx.isLoggedIn && (
-          <>
-            <Route path="*" element={<Navigate replace to="/auth" />} />
-            <Route path="/auth" element={<AuthPage />} />
-          </>
-        )}
+        <Route path="*" element={<Navigate replace to="/home" />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/add-recipe" element={<SearchPage />} />
       </Routes>
     </Layout>
   );
